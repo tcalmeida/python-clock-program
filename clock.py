ce5ceee60c2ff3626b4bdc7cdcd91c8b89b147bd
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-07-28 19:50:19 -0300

    create update_clock function to handle clock info updates

--- clock.py
+++ clock.py
@@ -1,7 +1,17 @@
 import time
 from tkinter import *
 
+def update_clock():
+    display_time = time.strftime('%H:%M:%S')
+    time_label.config(text=display_time)
 
+    display_day = time.strftime('%A')
+    day_label.config(text=display_day)
+
+    display_date = time.strftime('%B %d, %Y')
+    date_label.config(text=display_date)
+
+    window.after(1000, update_clock)
 
 
 
@@ -17,6 +27,6 @@ day_label.pack()
 date_label = Label(window, font=('Ubuntu', 25))
 date_label.pack()
 
-
+update_clock()
 
 window.mainloop()
